4fddebffcbf42ffe9ced7b9aafb8f08e6950b440
vladimir papazov <v.papazov@bucons.com>
2016-05-28 16:53:19 +0300

    no message

--- libs/custom.js
+++ libs/custom.js
@@ -44,7 +44,7 @@ $(window).load(function(){
 jQuery(document).ready(function() {
     jQuery('.viewport').addClass("view_hidden").viewportChecker({
 
-        classToAdd: 'visible animated  slideInUp  ',
+        classToAdd: 'visible animated  fadeIn  ',
         offset: 120
        });
    
@@ -58,11 +58,46 @@ jQuery(document).ready(function() {
 
 jQuery(document).ready(function() { 
 
+var winheight = $("html").height();
+var navheight= $("nav").height();
+
+
 
 //##### parallax init
 $('#scene').parallax();
 
 
+
+
+//### ICONS ACTIONS
+$(".iconcluster").click(function(){
+$(this).addClass("center ");
+$(".iconmain").addClass(" animated zoomIn").css("cursor","default").css("z-index",1);
+$(" .innericon").addClass(" animated zoomIn").css("opacity",1);
+$(".iconmain").css("z-index",2);
+
+$(".iconsholder").children().not(".center").addClass("dissapear").delay(400).queue(function(next){
+    $(this).css("display", "none");
+    $(".mainspan").css("display", "none");
+
+    next();
+    $(".iconcluster").addClass("center2");
+      $(".linkspecial").css("display","block");
+      $(".iconmain").css("z-index",1);
+});
+
+});
+
+
+
+$('.countdown').ClassyCountdown({
+    theme: "flat-colors-very-wide",
+    end: $.now() + 60,
+    onEndCallback: function(){
+        //alert("asdsadasd");
+    }
+});
+         
 
 
 });//end dready
