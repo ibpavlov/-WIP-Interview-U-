38ae5a32218b482684b46ea3dc1fc8f198e5b47c
Ivelin Pavlov <ibpavlov@gmail.com>
2016-05-29 15:55:43 +0300

    Contribution added. Scheme added and migration if you want to use phalcon

---
+++ app/migrations/1.0.2/questions.php
@@ -0,0 +1,100 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class QuestionsMigration_102
+ */
+class QuestionsMigration_102 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('questions', array(
+                'columns' => array(
+                    new Column(
+                        'id',
+                        array(
+                            'type' => Column::TYPE_INTEGER,
+                            'unsigned' => true,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 10,
+                            'first' => true
+                        )
+                    ),
+                    new Column(
+                        'name',
+                        array(
+                            'type' => Column::TYPE_VARCHAR,
+                            'size' => 500,
+                            'after' => 'id'
+                        )
+                    ),
+                    new Column(
+                        'ip',
+                        array(
+                            'type' => Column::TYPE_VARCHAR,
+                            'size' => 100,
+                            'after' => 'name'
+                        )
+                    ),
+                    new Column(
+                        'type',
+                        array(
+                            'type' => Column::TYPE_VARCHAR,
+                            'size' => 50,
+                            'after' => 'ip'
+                        )
+                    ),
+                    new Column(
+                        'created',
+                        array(
+                            'type' => Column::TYPE_TIMESTAMP,
+                            'default' => "CURRENT_TIMESTAMP",
+                            'size' => 1,
+                            'after' => 'type'
+                        )
+                    )
+                ),
+                'indexes' => array(
+                    new Index('PRIMARY', array('id'), null)
+                ),
+                'options' => array(
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '33',
+                    'ENGINE' => 'MyISAM',
+                    'TABLE_COLLATION' => 'utf8_general_ci'
+                ),
+            )
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
